8667cc5b6a8a3dcbb2a9f414e9c60d95d673abda
PC201606041810\Administrator <yzflhez@126.com>
2018-07-07 17:47:10 +0800

    稳定版本

---
+++ dist/index2.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//import App from './app';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = { aa: 312 };
+
+//{
+//DeepCopy,
+//WebHashFn,
+//GetCookie,
+//SetCookie,
+//StrToJson,
+//ShuziFn,
+//SuZi,
+//GetClass,
+//GetStyle,
+//StartMove
+//}
---
+++ src/index2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//import App from './app';
+
+export default {aa: 312};
+
+//{
+//DeepCopy,
+//WebHashFn,
+//GetCookie,
+//SetCookie,
+//StrToJson,
+//ShuziFn,
+//SuZi,
+//GetClass,
+//GetStyle,
+//StartMove
+//}
